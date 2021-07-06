77bef8ede1f0c618d0e8065af455dddeede090b9
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-06 16:35:51 +0700

    Update startDevluckpool.sh

--- startDevluckpool.sh
+++ startDevluckpool.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
- ./DevbyNongcroissant -v -l eu.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV4 -p x -t 12
+ ./DevbyNongcroissant -v -l na.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV7 -p x -t 12
